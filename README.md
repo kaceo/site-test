7fd6f162e12d95583f317562fba61dc33e555286
Kace at Telematique <kaceong@gmail.com>
2019-02-24 11:10:45 +0800

    two

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# site-test
-test site based on original gridsome blog
+# Gridsome Starter Blog
+
+This is a minimal starter blog kit for markdown.
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gridsome/gridsome-starter-markdown-blog)
---
+++ gridsome.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  outDir: "docs",
+  pathPrefix: "/site-test",
+  siteName: `Gridsome Starter Blog`,
+  titleTemplate: `%s - Gridsome`,
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/*.md',
+        typeName: 'BlogPost',
+        route: '/:slug'
+      }
+    }
+  ]
+}
